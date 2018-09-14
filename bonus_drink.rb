db4ddaab51d22b65929f6e9cfe331837e237b065
KtrWtnb <whatnowbay.k3nx2@gmail.com>
2018-09-14 15:36:46 +0900

    bonus-drink answers

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount < 3
+      amount
+    elsif amount % 3 == 0
+      bonus = amount / 3
+      amount + bonus
+    else
+      bonus = amount / 3
+      mod = amount % 3
+      amount + bonus + mod
+    end
   end
 end
